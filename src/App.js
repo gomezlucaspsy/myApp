02a570e95a809d627d4083ab4579e30eda2667df
LUCAS MARIANO GOMEZ VILLASUSO <lucas.gomez@mi.unc.edu.ar>
2023-12-31 13:13:23 -0300

    git push

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
       <h1>Welcome to the Air Traffic Control (ATC)</h1>
       {show &&
       <div>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/ywgAZw8JKeY?si=63NDsbi5Zm5Xbdyn" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width="400" height="225" src="https://www.youtube.com/embed/ywgAZw8JKeY?si=63NDsbi5Zm5Xbdyn" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       <iframe width="400" height="225" src="https://www.youtube.com/embed/IMtKOLghyk4?si=QFvbkUaa9GGVx6FE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/pQGIJ2vfK9c?si=YzKpBjT8KKp7jP5_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/lSRvsfr-kpg?si=G8uK_k_ELkuWqn4w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/HfgIFGbdGJ0?si=85IjtI46RjZ7_9hV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width="400" height="225" src="https://www.youtube.com/embed/pQGIJ2vfK9c?si=YzKpBjT8KKp7jP5_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width="400" height="225" src="https://www.youtube.com/embed/lSRvsfr-kpg?si=G8uK_k_ELkuWqn4w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width="400" height="225" src="https://www.youtube.com/embed/HfgIFGbdGJ0?si=85IjtI46RjZ7_9hV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
       }
       <button class="round" type='button' onClick={()=>setShow(!show)}> 
