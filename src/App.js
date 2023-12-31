39780d88708d6cc098538f45867d700a9002e18f
LUCAS MARIANO GOMEZ VILLASUSO <lucas.gomez@mi.unc.edu.ar>
2023-12-31 12:07:43 -0300

    atc

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log("Talk with the admin https://docs.google.com/forms/d/1yQ1KlpqsmpJkstexPIPYhxL3I6GgobPtULhDGZxAc2s")
   return (
     <div className="App">
       <header className="App-header">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/pQGIJ2vfK9c?si=YzKpBjT8KKp7jP5_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>        <img src={logo} className="App-logo" alt="logo" />
+      <h1>Welcome to the Air Traffic Control (ATC)</h1>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/t0GrpAgdBFI?si=BpN0LhHt7Z3rxUom" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/pQGIJ2vfK9c?si=YzKpBjT8KKp7jP5_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </header>
     </div>
   );
