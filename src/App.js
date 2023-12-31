aa6edc8ac232c28ec127d5cb9de5cfc3c6b21884
LUCAS MARIANO GOMEZ VILLASUSO <lucas.gomez@mi.unc.edu.ar>
2023-12-31 10:44:32 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -5,11 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/pQGIJ2vfK9c?si=py2Mm7UAgEj-gcK1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/pQGIJ2vfK9c?si=py2Mm7UAgEj-gcK1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>      </header>
+      </header>
     </div>
   );
 }
