d6181d36303017cebf4ccaaec421091ab6f237c2
LUCAS MARIANO GOMEZ VILLASUSO <lucas.gomez@mi.unc.edu.ar>
2024-01-02 12:50:26 -0300

    Cameras could be a good option

--- src/App.js
+++ src/App.js
@@ -11,6 +11,16 @@ function App() {
       <h1>Welcome to the Air Traffic Control (ATC)</h1>
       {show &&
       <div className='ControlPanel'>
+      <ul>
+        <li><a href="" className='App-link'>1</a></li>
+        <li><a href="" className='App-link'>2</a></li>
+        <li><a href="" className='App-link'>3</a></li>
+        <li><a href="" className='App-link'>4</a></li>
+        <li><a href="" className='App-link'>5</a></li>
+        <li><a href="" className='App-link'>6</a></li>
+        <li><a href="" className='App-link'>7</a></li>
+        <li><a href="" className='App-link'>8</a></li>
+      </ul>
       <iframe width="400" height="225" src="https://www.youtube.com/embed/ywgAZw8JKeY?si=63NDsbi5Zm5Xbdyn" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       <iframe width="400" height="225" src="https://www.youtube.com/embed/IMtKOLghyk4?si=QFvbkUaa9GGVx6FE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       <iframe width="400" height="225" src="https://www.youtube.com/embed/lSRvsfr-kpg?si=G8uK_k_ELkuWqn4w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
