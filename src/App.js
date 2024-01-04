4682b49968c82589ff40d3cd3737551a03ed41ea
LUCAS MARIANO GOMEZ VILLASUSO <lucas.gomez@mi.unc.edu.ar>
2024-01-04 14:15:44 -0300

    less a

--- src/App.js
+++ src/App.js
@@ -17,10 +17,7 @@ function App() {
   <path d="M0 7.76C0 3.301 3.493 0 8 0s8 3.301 8 7.76-3.493 7.76-8 7.76c-.81 0-1.586-.107-2.316-.307a.639.639 0 0 0-.427.03l-1.588.702a.64.64 0 0 1-.898-.566l-.044-1.423a.639.639 0 0 0-.215-.456C.956 12.108 0 10.092 0 7.76m5.546-1.459-2.35 3.728c-.225.358.214.761.551.506l2.525-1.916a.48.48 0 0 1 .578-.002l1.869 1.402a1.2 1.2 0 0 0 1.735-.32l2.35-3.728c.226-.358-.214-.761-.551-.506L9.728 7.381a.48.48 0 0 1-.578.002L7.281 5.98a1.2 1.2 0 0 0-1.735.32z"/>
 </svg>Admin</a></li>
       <li><a href="https://youtu.be/1esTygT7upo" className='App-link'>ATC ARGENTINA</a></li>
-        <li><a href="" className='App-link'>4</a></li>
-        <li><a href="" className='App-link'>5</a></li>
-        <li><a href="" className='App-link'>6</a></li>
-        <li><a href="" className='App-link'>7</a></li>
+        <li><a href="" className='App-link'>2</a></li>
         <li><a href="https://files.ar.ivao.aero/Training/Manuales/Fraseologia.pdf" className='App-link'>Dictionary ES</a></li>
         <input type="text" placeholder="Search.."></input>
         </ul>
